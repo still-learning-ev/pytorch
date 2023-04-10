0ae328d8555a0cf7d04ec70559149978a85be474
zeeshan <through.learning@gmail.com>
2023-04-10 23:54:28 +0530

    modified

--- fashionMNIST/app.py
+++ fashionMNIST/app.py
@@ -3,21 +3,20 @@ from PIL import Image
 from predict import predict_image
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def upload_page():
-    return render_template('index.html')
-
-@app.route('/', methods = ['POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def predict():
-    image_file = request.files['image']
-    try:
-        image = Image.open(image_file)
-    except Exception as e:
-        return render_template('error.html', error=e)
-    
-    predicted_label, predict_class = predict_image(image)
-    
-    return render_template('index.html', label=predicted_label, predict_class=predict_class)
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        image_file = request.files['image']
+        try:
+            image = Image.open(image_file)
+        except Exception as e:
+            return render_template('error.html', error=e)
+        
+        predicted_label, predict_class = predict_image(image)
+        
+        return render_template('index.html', label=predicted_label, predict_class=predict_class)
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
